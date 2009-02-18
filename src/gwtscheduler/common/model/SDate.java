ea3c81eb60cd6a16defa28e5ace3740cc9062abe
miguel.ping <devnull@localhost>
2009-02-18 15:46:56 +0000

    [svn r207] - Removed unecessary @Override annotation for interfaces (not compatible with J2SE5 sdk)

--- src/gwtscheduler/common/model/SDate.java
+++ src/gwtscheduler/common/model/SDate.java
@@ -11,72 +11,65 @@ import java.util.Date;
  */
 public class SDate implements IDate, Comparable<IDate> {
 
-	/**
-	 * Internal date field.
-	 */
-	private Date delegate;
+    /**
+     * Internal date field.
+     */
+    private Date delegate;
 
-	/**
-	 * Default constructor.
-	 */
-	public SDate() {
-		delegate = new Date();
-	}
+    /**
+     * Default constructor.
+     */
+    public SDate() {
+        delegate = new Date();
+    }
 
-	@Override
-	public Object delegate() {
-		return delegate;
-	}
+    public Object delegate() {
+        return delegate;
+    }
 
-	@Override
-	public IDate addDays(int amount) {
-		return this;
-	}
+    public IDate addDays(int amount) {
+        return this;
+    }
 
-	@Override
-	public IDate addMonths(int amount) {
-		return this;
-	}
+    public IDate addMonths(int amount) {
+        return this;
+    }
 
-	@Override
-	public IDate addYears(int amount) {
-		return this;
-	}
+    public IDate addYears(int amount) {
+        return this;
+    }
 
-	@Override
-	public int diff(IDate otherDate, TimeUnit unit) {
-		return 0;
-	}
+    public int diff(IDate otherDate, TimeUnit unit) {
+        return 0;
+    }
 
-	@Override
-	public int diff(IDate otherDate) {
-		return 0;
-	}
+    public int diff(IDate otherDate) {
+        return 0;
+    }
 
-	@Override
-	public int compareTo(IDate o) {
-		return delegate.compareTo((Date) o.delegate());
-	}
+    public int compareTo(IDate o) {
+        return delegate.compareTo((Date) o.delegate());
+    }
 
-	/**
-	 * Truncates this date instance.
-	 * 
-	 * @param field the field to truncate
-	 * @return this instance
-	 */
-	protected IDate truncate(int field) {
-		// TODO implement me
-		boolean done = false;
-		if (MILLISECOND == field) {
-			done = true;
-		}
-		if (SECOND == field) {
-			done = true;
-		}
-		if (MINUTE == field) {
-			done = true;
-		}
-		return this;
-	}
+    /**
+     * Truncates this date instance.
+     * 
+     * @param field the field to truncate
+     * @return this instance
+     */
+    protected IDate truncate(int field) {
+        // TODO implement me
+        boolean done = false;
+        if (MILLISECOND == field) {
+            done = true;
+        }
+        if (SECOND == field) {
+            done = true;
+        }
+        if (MINUTE == field) {
+            done = true;
+        }
+        return this;
+    }
 
 }
