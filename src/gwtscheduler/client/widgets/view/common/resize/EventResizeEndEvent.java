a8ddca535f197419c1ff1ed59ae4a85e4b8164ce
Lazo Apostolovski [lazo.apostolovski@gmail.com] <Administrator@.(none)>
2010-03-17 09:01:12 +0200

    Comited invalid file

---
+++ src/gwtscheduler/client/widgets/view/common/resize/EventResizeEndEvent.java
@@ -0,0 +1,8 @@
+package gwtscheduler.client.widgets.view.common.resize;
+
+/**
+ * @author Lazo Apostolovski (lazo.apostolovski@gmail.com)
+ */
+public class EventResizeEndEvent {
+  // TODO: DELETE
+}
